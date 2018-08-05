3d755a03c87d016f4e6656a64de625d09301fa8a
Rnd-5 <rondcapbel@gmail.com>
2018-08-04 20:58:56 -0400

    Primer Commit C_Node

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales')
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    /* res.send('Hello World');
+    let salida = {
+        nombre: 'Fernando',
+        edad: 32,
+        url: req.url};*/
+
+    res.render('home', {
+        name: 'fernando'
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('parciales/about');
+
+});
+
+/*
+app.get('/data', (req, res) => {
+
+    res.send('Hello Data');
+
+});
+*/
+
+app.listen(port, () => {
+    console.log(`Escuchando petisiones en el puerto ${ port }.`);
+});
