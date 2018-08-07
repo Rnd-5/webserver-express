020fa26d5803af06ecb19882cd8cbf73d2628528
Rnd-5 <rondcapbel@gmail.com>
2018-08-07 14:45:36 -0400

    4to commit de esta rama(he agragado varias mejoras en el aspecto visual de la pag web, junto a los formularios post y put de publicacion y edicion de noticias respectivamente)

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 //====================REQUIRES_Y_CONST====================//
-const express = require('express');
-const app = express();
-
-const hbs = require('hbs');
+require('./config/config');
 require('./hbs/helpers');
 
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'));
-
 
 //Express HBS engine
 hbs.registerPartials(__dirname + '/views/parciales')
 app.set('view engine', 'hbs');
 
 //=============================================================//
+
+mongoose.connect('mongodb://localhost:27017/BD_Noticias_com', (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+});
 
 //========================PETICIONES_GET======================//
 app.get('/', (req, res) => {
@@ -44,11 +55,7 @@ app.get('/vista_noticia', (req, res) => {
 
 });
 
-app.get('/publicar_noticia', (req, res) => {
 
-    res.render('parciales/publicar_noticia');
-
-});
 
 /*
 app.get('/data', (req, res) => {
@@ -61,8 +68,22 @@ app.get('/data', (req, res) => {
 
 //========================PETICIONES_POST======================//
 
+app.get('/publicar_noticia', function(req, res) {
+
+    res.render('parciales/formulario_noticia');
+
+});
 
 
+//=============================================================//
+
+//========================PETICIONES_PUT======================//
+
+app.get('/editar_noticia', function(req, res) {
+
+    res.render('parciales/editar_noticia');
+
+});
 
 
 //=============================================================//
@@ -70,5 +91,5 @@ app.get('/data', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Escuchando petisiones en el puerto ${ port }.`);
+    console.log('Escuchando petisiones en el puerto:', port);
 });
